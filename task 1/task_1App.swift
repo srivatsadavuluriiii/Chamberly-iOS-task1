f1f066ad8580ec49e01c6a3ead6563484ae23796
srivatsadavuluriiii <xstcy@protonmail.com>
2024-01-16 21:14:46 +0530

    Initial Commit

---
+++ task 1/task_1App.swift
@@ -0,0 +1,17 @@
+//
+//  task_1App.swift
+//  task 1
+//
+//  Created by thrxmbxne on 16/01/24.
+//
+
+import SwiftUI
+
+@main
+struct task_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
